75227be8a88781be7daf4a1fa0ed0129ffe6e7dd
Nikita Prokopenko <guardprokopenko@gmail.com>
2017-11-18 17:16:25 +0300

    Bootstrab.scss file is converted
    Converted bootstrap.scss file to the bootstrap.css and reconnected styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
+gulp.task('sass', function() {
   // place code for your default task here
+	return gulp.src('./node_modules/bootstrap/scss/bootstrap.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./css'));
 });
+
+gulp.task('sass:watch', function () {
+	gulp.watch('./node_modules/bootstrap/scss/bootstrap.scss', ['sass']);
+  });
