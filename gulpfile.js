ebeb7ee0d0fa785c70bb2e3ed8dc80f3a9b57684
Nikita Prokopenko <guardprokopenko@gmail.com>
2017-11-26 17:19:55 +0300

    Added twigjs

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var twig = require('gulp-twig');
 
 gulp.task('sass', function() {
-  // place code for your default task here
+  	// place code for your default task here
 	return gulp.src('./node_modules/bootstrap/scss/bootstrap.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./css'));
@@ -10,4 +11,10 @@ gulp.task('sass', function() {
 
 gulp.task('sass:watch', function () {
 	gulp.watch('./node_modules/bootstrap/scss/bootstrap.scss', ['sass']);
-  });
+});
+
+gulp.task('twig', function() {
+	return gulp.src('content/**/*.twig')
+		.pipe(twig())
+		.pipe(gulp.dest('./dist'));
+});
