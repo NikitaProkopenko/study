1b85e4b8ca5189802425cc03f61b4c0753e66551
Nikita Prokopenko <guardprokopenko@gmail.com>
2017-11-27 22:58:39 +0300

    Revert "Merge pull request #3 from NikitaProkopenko/twigJS"
    This reverts commit 91648434d90814fb4d291c50e295b6c63edf721b.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var twig = require('gulp-twig');
 
 gulp.task('sass', function() {
-  	// place code for your default task here
+  // place code for your default task here
 	return gulp.src('./node_modules/bootstrap/scss/bootstrap.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./dist/css'));
+		.pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function () {
 	gulp.watch('./node_modules/bootstrap/scss/bootstrap.scss', ['sass']);
-});
-
-gulp.task('twig', function() {
-	return gulp.src('content/**/*.twig')
-		.pipe(twig())
-		.pipe(gulp.dest('./dist'));
-});
+  });
