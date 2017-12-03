37edad7a73be94172bf7accfc46be90dba78eb71
Nikita Prokopenko <guardprokopenko@gmail.com>
2017-12-03 21:07:06 +0300

    Answers script
    - Added script for answers parsing
    - Added "name" attribute for inputs on the exercise 1 page

--- script/submitAnswer.js
+++ script/submitAnswer.js
@@ -1,7 +1,33 @@
-function submitAnswer() {
-    var str = $( "form" ).serialize();
-    console.log(str);
-  }
+console.log(window.location.href);
+function submitAnswer(event) {
+	event.preventDefault();
+	var str = $("form").serialize();
+	console.log($(this).serialize());
+	var url = window.location.href;
+	console.log(url);
+	var result = url + "?" + str;
+	console.log(result);
+}
+$("form").on("submit", submitAnswer);
 
-$("submit").on("click", submitAnswer);
-  showValues();
+function parseUrl() {
+	var url = window.location.href;
+	var arr = url.split("?");
+	console.log(arr);
+	var answerArr = arr[1].split("&");
+	console.log(answerArr);
+
+	var answerKeyValue = [];
+	for (var i = 0; i < answerArr.length; i++) {
+		var key = answerArr[i].split("=");
+		answerKeyValue.push(key);
+	}
+	console.log(answerKeyValue);
+
+	for (var x = 0; x < answerKeyValue.length; x++) {
+		var text = answerKeyValue[x][1];
+		console.log(text);
+		$("#" + answerKeyValue[x][0]).val(text);
+	}
+}
+parseUrl();
